eeb044a087543e81c423c7dec6f7ee90f5f9b952
Martin Cabral <cabral.martin@gmail.com>
2018-09-14 16:16:06 -0300

    WIP - Trying to delete cookie from background script

--- extension/contentScript.js
+++ extension/contentScript.js
@@ -1,5 +1,7 @@
 (() => {
 
+  const TIMEOUT = 3500;
+
   var scriptContent = `
     const TIMEOUT = 3000;
     console.log('Loading magic in ', TIMEOUT);
@@ -19,4 +21,11 @@
     script.appendChild(document.createTextNode(scriptContent));
     (document.body || document.head || document.documentElement).appendChild(script);
   
+
+  // Signal background script to delete cookies
+  // This needs to happen AFTER the injected script runs
+  // Otherwise the cookies will be regenerated
+  setTimeout(() => {
+    chrome.runtime.sendMessage({ 1:1 });
+  }, TIMEOUT)
 })();
